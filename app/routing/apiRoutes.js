43a4730be277b05d2cfbee240d966201b817bc14
Tphorton14 <Tphorton14@gmail.com>
2019-08-19 11:40:04 -0400

    api routing continue

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,22 @@
+const home = require('./public/home.html');
+
+const survey = require("./public/survey.html");
+
+
+module.exports = function (app) {
+
+//routing home
 app.get("/api/friends", function (req, res){
-    return res.JSON('./public/survey.html')
+    return res.JSON(home);
 });
 
+//routing to survey??
+// app.get("/api/", function (req, res){
+//     return res.JSON(survey);
+// });
+
 app.post("/api/friends", function(req, res) {
+
+}
 
 }
