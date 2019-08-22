80809c5658dedac1025f0b67c6b43ba1f886f86c
Tphorton14 <Tphorton14@gmail.com>
2019-08-22 11:28:02 -0400

    full project update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,18 +5,30 @@ const survey = require("./public/survey.html");
 
 module.exports = function (app) {
 
-//routing home
-app.get("/api/friends", function (req, res){
-    return res.JSON(home);
-});
+    //routing home
+    app.get("/api/friends", function (req, res) {
+        return res.JSON(home);
+    });
 
-//routing to survey??
-// app.get("/api/", function (req, res){
-//     return res.JSON(survey);
-// });
 
-app.post("/api/friends", function(req, res) {
-    home.length = 0;
-}
+    // routing survey
+    app.get("/api/friends", function (req, res) {
+        return res.JSON(survey);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+
+        if (survey){
+            survey.push(req.body)
+            return res.json(true);
+        } else {
+            res.json(false);
+        }
+        
+
+        //req.body = {userData}
+
+    });
 
 }
